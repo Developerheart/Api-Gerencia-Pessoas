e0e8bdc6e6e7b97f6979a4f87ef603fe31298701
Developerheart <62905562+Developerheart@users.noreply.github.com>
2021-09-08 17:49:23 -0300

    Criado a base do projeto

---
+++ src/main/java/com/br/tec/gerenciamanagent/GrecianManagementApplication.java
@@ -0,0 +1,13 @@
+package com.br.tec.gerenciamanagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrecianManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrecianManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/br/tec/gerenciamanagent/GerenciaManagentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.tec.gerenciamanagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciaManagentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
